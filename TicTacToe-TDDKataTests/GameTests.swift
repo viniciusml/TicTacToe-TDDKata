aeaf9b887bd77dd1e147cafa8ef7f0f58be3860c
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-07-04 14:13:42 +0200

    On game initialization, each player receives a unique valid spot option

--- TicTacToe-TDDKataTests/GameTests.swift
+++ TicTacToe-TDDKataTests/GameTests.swift
@@ -12,10 +12,15 @@ import XCTest
 class Game {
 
     let board: Board
+    let player1: SpotOption
+    let player2: SpotOption
+
     private(set) var isActive: Bool = false
 
     init(board: Board) {
         self.board = board
+        self.player1 = .cross
+        self.player2 = .nought
     }
 
     func start() {
@@ -29,6 +34,14 @@ class GameTests: XCTestCase {
         let (sut, _) = makeSUT()
 
         XCTAssertFalse(sut.isActive)
+    }
+
+    func test_init_playersReceiveDifferentInputOptionTypes() {
+        let (sut, _) = makeSUT()
+
+        XCTAssertNotEqual(sut.player1, .none)
+        XCTAssertNotEqual(sut.player2, .none)
+        XCTAssertNotEqual(sut.player1, sut.player2)
     }
 
     func test_start_setsGameToActive() {
