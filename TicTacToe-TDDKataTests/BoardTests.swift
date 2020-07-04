1a869df254dafd85a8512c2993831d335a45c647
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-07-04 13:32:02 +0200

    Board is initialised with 9 available spots

---
+++ TicTacToe-TDDKataTests/BoardTests.swift
@@ -0,0 +1,27 @@
+//
+//  BoardTests.swift
+//  TicTacToe-TDDKataTests
+//
+//  Created by Vinicius Moreira Leal on 04/07/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+
+class Board {
+
+    let spots: [Int]
+
+    init() {
+        spots = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    }
+
+}
+
+class BoardTests: XCTestCase {
+
+    func test_init_boardHasNineSpots() {
+        let sut = Board()
+        XCTAssertEqual(sut.spots.count, 9)
+    }
+}
