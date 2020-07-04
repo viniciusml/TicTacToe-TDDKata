146c630435444263a244a48f6daa84fdfd31e937
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-07-04 13:16:01 +0200

    Project cleanup

---
+++ TicTacToe-TDDKataTests/TicTacToeTests.swift
@@ -0,0 +1,16 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToe-TDDKataTests
+//
+//  Created by Vinicius Moreira Leal on 04/07/2020.
+//  Copyright © 2020 Vinicius Moreira Leal. All rights reserved.
+//
+
+import XCTest
+
+class TicTacToeTests: XCTestCase {
+
+    func test() {
+        
+    }
+}
