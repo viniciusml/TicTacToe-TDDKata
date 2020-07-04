4097dbe43a69c631d1cc179d1c85082d03ec45ac
Vinicius Leal <viniciusmoreiraleal@gmail.com>
2020-07-04 13:41:44 +0200

    Board accepts input in correct location

--- TicTacToe-TDDKataTests/BoardTests.swift
+++ TicTacToe-TDDKataTests/BoardTests.swift
@@ -10,12 +10,15 @@ import XCTest
 
 class Board {
 
-    let spots: [Int]
+    var spots: [Int]
 
     init() {
         spots = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 
+    func receive(_ input: Int, at index: Int) {
+        spots[index] = input
+    }
 }
 
 class BoardTests: XCTestCase {
@@ -28,6 +31,15 @@ class BoardTests: XCTestCase {
     func test_init_boardHasAllSpotsAvailable() {
         let sut = Board()
         XCTAssertTrue(sut.hasAllSpotsAvailable)
+    }
+
+    func test_board_acceptsGameInputOnAvailableSpots() {
+        let sut = Board()
+
+        sut.receive(1, at: 3)
+
+        XCTAssertFalse(sut.hasAllSpotsAvailable)
+        XCTAssertEqual(sut.spots[3], 1)
     }
 }
 
